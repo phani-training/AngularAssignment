3040b572456c77248a0682185d04870f7f21cd77
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-08-02 15:31:41 +0530

    Add files via upload

---
+++ src/app/Services/patient.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Patient } from '../Models/patient';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+  patients : Patient[] = [];
+  constructor() { 
+    this.patients.push({id: 111, address : "Bangalore", bill : 5000, doctor :"Dr. Nitesh Tiwari", name: 'Phaniraj'});
+    this.patients.push({id: 112, address : "Mysore", bill : 15000, doctor :"Dr. Srujanna", name: 'Somnath'});
+    this.patients.push({id: 113, address : "Pune", bill : 2000, doctor :"Dr. SV ", name: 'Kumar'});
+    this.patients.push({id: 114, address : "Bangalore", bill : 5000, doctor :"Dr. Anirudha", name: 'Lavanya'});
+  }
+
+  addNewPatient(p : Patient){
+    this.patients.push(p);
+  }
+
+  removePatient(id : number){
+    let index = this.patients.findIndex((p)=>p.id == id);
+    this.patients.splice(index, 1);  
+  }
+}
